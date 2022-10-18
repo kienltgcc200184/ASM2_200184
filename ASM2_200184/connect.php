c233b90b7e29c83f1577870628b28c64398bd004
kienltgcc200184 <kienltgcc200184@fpt.edu.vn>
2022-10-18 09:05:37 +0700

    change conn

--- ASM2_200184/connect.php
+++ ASM2_200184/connect.php
@@ -1,13 +1,18 @@
 <?php
 
-$server = "localhost";
-$username ="root";
-$password = "";
-$db = "shop_200184";
+// $server = "localhost";
+// $username ="root";
+// $password = "";
+// $db = "shop_200184";
 
-$conn = mysqli_connect($server,$username,$password,$db);
-if($conn->connect_error){
-    die("Failed ".$conn->connect_error);
+// $conn = mysqli_connect($server,$username,$password,$db);
+// if($conn->connect_error){
+//     die("Failed ".$conn->connect_error);
+// }
+$conn = pg_connect("postgres://nugkvsyrzekppd:984299e482ad7ad28e81b9ec8e369deee8db6cb0d5eedd89137ff9614f012bc1@ec2-54-211-255-161.compute-1.amazonaws.com:5432/d38ub39isp8f0r");
+
+if(!$Conn){
+    die("conn failed");
 }
 
 ?>
